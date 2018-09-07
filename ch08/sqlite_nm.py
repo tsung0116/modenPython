0ef1c95252b8b46cd11aa9d027c2b0197810bafc
tsung0116 <mingtzung.hsu@gmail.com>
2018-09-07 16:45:51 +0800

    20180907-1

--- ch08/sqlite_nm.py
+++ ch08/sqlite_nm.py
@@ -5,3 +5,6 @@ curs.execute("SELECT name FROM sqlite_master WHERE type='table';")
 print(curs.fetchall())
 curs.execute("select sql from sqlite_master where type = 'table' and name = 'mail';")
 print(curs.fetchall())
+
+curs.close()
+conn.close()
